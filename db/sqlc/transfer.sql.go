d72d92b3176f53c207280a830a2321b468a58c32
hualinli <749930355@qq.com>
2026-03-31 16:34:59 +0800

    fix: initialize slices for account, entry, and transfer queries

--- db/sqlc/transfer.sql.go
+++ db/sqlc/transfer.sql.go
@@ -84,7 +84,7 @@ func (q *Queries) ListTransfers(ctx context.Context, arg ListTransfersParams) ([
 		return nil, err
 	}
 	defer rows.Close()
-	var items []Transfer
+	items := []Transfer{}
 	for rows.Next() {
 		var i Transfer
 		if err := rows.Scan(
@@ -123,7 +123,7 @@ func (q *Queries) ListTransfersByFromAccount(ctx context.Context, arg ListTransf
 		return nil, err
 	}
 	defer rows.Close()
-	var items []Transfer
+	items := []Transfer{}
 	for rows.Next() {
 		var i Transfer
 		if err := rows.Scan(
@@ -162,7 +162,7 @@ func (q *Queries) ListTransfersByToAccount(ctx context.Context, arg ListTransfer
 		return nil, err
 	}
 	defer rows.Close()
-	var items []Transfer
+	items := []Transfer{}
 	for rows.Next() {
 		var i Transfer
 		if err := rows.Scan(
